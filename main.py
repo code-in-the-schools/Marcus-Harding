80ff70fc68c2eba2fd8bdd55d742895ba633b5cb
marcus2x <wowmarc11@gmail.com>
2020-07-15 16:26:26 +0000

    variables

---
+++ main.py
@@ -0,0 +1,17 @@
+print ("hello world")
+name = "Marcus Harding"
+print (name)
+favartist = "Nba Youngnoy"
+print (favartist)
+favsport = "Basketball"
+print (favsport)
+vacation = "Domincan Republic"
+print (vacation)
+date = "july 14th 2020"
+print (date)
+a= name
+b= favartist
+c=favsport
+d=vacation
+print("My name is"+ a and"My favorite artist is "+b)
+print ("I enjoy to play"+c and "Would love to visit "+d)
